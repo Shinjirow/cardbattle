5d188cb74ab0e7cbd897b5b9baa69f0d871f72d1
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-05-25 16:49:33 +0900

    add skelton

---
+++ cardbattle/E2/Play.java
@@ -0,0 +1,24 @@
+package E2;
+
+public class Play implements P.Player {
+
+	public int call (
+		int[] mycard, int[] yourcard, 
+		int[] mylog, int[] yourlog, 
+		double mylastscore, double yourlastscore, 
+		double mytotalscore, double yourtotalscore, 
+		int count, long timeleft
+	)
+	{
+		int submit = 0;
+		java.util.Random r = new java.util.Random();
+		while (true) {
+			submit = r.nextInt(13) + 1;
+			if (mycard[submit]==0) {
+				return submit;
+			}
+		}
+
+	}
+
+}
